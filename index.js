7e1d3c98fcc612cd2b0ce2746b5504aa453ab0d7
GioProgramming6 <gioravaxar12@gmail.com>
2025-03-15 00:20:17 +0400

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world! kk")
+let count = 0;
+
+document.getElementById("increase").addEventListener("click", function () {
+    count++;
+    updateCounter();
+});
+
+document.getElementById("decrease").addEventListener("click", function () {
+    count--;
+    updateCounter();
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+    count = 0;
+    updateCounter();
+});
+
+function updateCounter() {
+    document.getElementById("count").textContent = count;
+}
